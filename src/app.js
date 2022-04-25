74d3a6943d1a42f15193ef71a9a71de60bc915ee
hokusai92 <99332718+hokusai92@users.noreply.github.com>
2022-04-25 18:16:37 +0900

    Showing temperature conditions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 function displayTemperature(response) {
-  console.log(response.data.main);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "13c5af97a2b76028ba01eb2e5a289080";
